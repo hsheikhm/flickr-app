b74e178d3cf43302ad152273e5daeb5aa668aca1
Hamza Sheikh <hsheikhm@gmail.com>
2016-04-20 19:11:03 +0100

    created factory, Feed to make get request to flickr api

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -1,19 +1,12 @@
 var flickrAppControllers = angular.module('flickrAppControllers', []);
 
-flickrAppControllers.controller('FeedCtrl', ['$http',
-  function($http){
+flickrAppControllers.controller('FeedCtrl', ['Feed',
+  function(Feed){
 
     var self = this;
 
-    var url = 'https://api.flickr.com/services/feeds/photos_public.gne';
-
-    $http.jsonp('https://api.flickr.com/services/feeds/photos_public.gne?tags=potato&tagmode=all&format=json', {
-        params: {
-            format: 'json',
-            jsoncallback: 'JSON_CALLBACK'
-        }
-    }).then(function(response){
-      console.log(response);
+    Feed.getAll().then(function(response){
+      self.list = response.data.items;
     });
 
   }
--- app/js/services.js
+++ app/js/services.js
@@ -1,1 +1,20 @@
 var flickrAppServices = angular.module('flickrAppServices', []);
+
+flickrAppServices.factory('Feed', ['$http',
+  function($http){
+
+    var url = 'https://api.flickr.com/services/feeds/photos_public.gne?tags=potato&tagmode=all&format=json';
+
+    return {
+      getAll: function(){
+        return $http.jsonp(url, {
+          params: {
+            format: 'json',
+            jsoncallback: 'JSON_CALLBACK'
+          }
+        });
+      }
+    };
+
+  }
+]);
