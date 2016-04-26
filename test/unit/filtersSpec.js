856dbb7d93f8361a4bb73a6fcc8eb513ce1bbb6b
Hamza Sheikh <hsheikhm@gmail.com>
2016-04-26 13:23:34 +0100

    completed all testing

--- test/unit/filtersSpec.js
+++ test/unit/filtersSpec.js
@@ -10,7 +10,7 @@
       it("should convert a date and time string to a custom format",
         inject(function(customDateFormatFilter){
           expect(customDateFormatFilter("2016-04-25T14:57:03Z")).toBe("25th Apr 2016 at 15:57");
-        }));
+      }));
 
     });
 
@@ -19,7 +19,7 @@
       it("should extract the author's actual name from the name string",
         inject(function(authorNameFilter){
           expect(authorNameFilter("nobody@flickr.com (rose ǝsoɹ)")).toBe("rose ǝsoɹ");
-        }));
+      }));
 
     });
 
