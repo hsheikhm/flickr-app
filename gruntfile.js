ce8946a0007fb4819df279dfc2962ee90f8d9419
Hamza Sheikh <hsheikhm@gmail.com>
2016-05-02 17:01:23 +0100

    completed grunt automation

--- gruntfile.js
+++ gruntfile.js
@@ -5,10 +5,35 @@
     grunt.initConfig({
       pkg: grunt.file.readJSON("package.json"),
 
-      watch: {
-        sass: {
-          files: "app/css/sass/*.sass",
-          tasks: ['sass']
+      concat: {
+        options: {
+          separator: ';'
+        },
+        dist: {
+          src: ['app/js/*.js'],
+          dest: 'dist/<%= pkg.name %>.js'
+        }
+      },
+
+      uglify: {
+        options: {
+          banner: '/*! <%= pkg.name %> <%= grunt.template.today("dd-mm-yyyy") %> */\n'
+        },
+        dist: {
+          files: {
+            'dist/<%= pkg.name %>.min.js': ['<%= concat.dist.dest %>']
+          }
+        }
+      },
+
+      jshint: {
+        files: ['gruntfile.js', 'app/js/*.js', 'test/**/*.js'],
+        options: {
+          globals: {
+            jQuery: true,
+            console: true,
+            module: true
+          }
         }
       },
 
@@ -18,13 +43,27 @@
             "app/css/master.css" : "app/css/sass/master.sass"
           }
         }
+      },
+
+      watch: {
+        sass: {
+          files: "app/css/sass/*.sass",
+          tasks: ['sass']
+        },
+        app: {
+          files: ['<%= jshint.files %>'],
+          tasks: ['jshint']
+        }
       }
     });
 
     grunt.loadNpmTasks('grunt-contrib-sass');
     grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-contrib-concat');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-jshint');
 
-    grunt.registerTask('default', ['sass']);
+    grunt.registerTask('default', ['jshint', 'concat', 'uglify', 'sass']);
 
   };
 
